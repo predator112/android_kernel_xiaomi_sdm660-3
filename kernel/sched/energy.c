130b8d3cf2810a90464e84321cd9ef97f503b0b0
Joonwoo Park <joonwoop@codeaurora.org>
2017-06-09 14:06:54 -0700

    sched: energy: calculate and update CPU capacity dynamically
    One SoC can have multiple CPU speedbins which cannot be represented
    with current energy model due to fixed capacity per CPU frequency
    steps.
    
    Provide CPU's all possible frequency steps instead of capacities along
    with corresponding energy costs to be able to support different
    speedbins.
    
    Change-Id: I96ff01372da5c383cd3172999ea1dcf95a7862ce
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- kernel/sched/energy.c
+++ kernel/sched/energy.c
@@ -137,7 +137,7 @@ static int sched_energy_probe(struct platform_device *pdev)
 	unsigned long *max_frequencies = NULL;
 	int ret;
 
-	if (!sched_feat(ENERGY_AWARE))
+	if (!sched_is_energy_aware())
 		return 0;
 
 	max_frequencies = kmalloc_array(nr_cpu_ids, sizeof(unsigned long),
