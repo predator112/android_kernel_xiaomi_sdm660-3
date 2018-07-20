bc6504a6f462560a780e15b7d0840db4b98fb0d1
Haseo97 <jawgattus@gmail.com>
2018-07-20 12:11:16 +0700

    powersuspend: Update to the latest version
    Signed-off-by: Haseo97 <jawgattus@gmail.com>

--- kernel/power/autosleep.c
+++ kernel/power/autosleep.c
@@ -18,10 +18,6 @@
 #include <linux/powersuspend.h>
 #endif
 
-#ifdef CONFIG_POWERSUSPEND
-#include <linux/powersuspend.h>
-#endif
-
 #include "power.h"
 
 static suspend_state_t autosleep_state;
@@ -118,15 +114,13 @@ int pm_autosleep_set_state(suspend_state_t state)
 		pm_wakep_autosleep_enabled(true);
 		queue_up_suspend_work();
 #ifdef CONFIG_POWERSUSPEND
-		// Yank555.lu : add hook to handle powersuspend tasks (sleep)
-		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_ACTIVE);
+		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_ACTIVE); // Yank555.lu : add hook to handle powersuspend tasks
 #endif
 	} else {
 		pm_wakep_autosleep_enabled(false);
 #ifdef CONFIG_POWERSUSPEND
-		// Yank555.lu : add hook to handle powersuspend tasks (wakeup)
-		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_INACTIVE);
-#endif
+		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_INACTIVE); // Yank555.lu : add hook to handle powersuspend tasks
+#endif		
 	}
 
 	mutex_unlock(&autosleep_lock);
