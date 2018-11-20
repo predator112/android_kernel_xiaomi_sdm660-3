1d973ffa368e895ebc1aca7a3e22ef8b753aacf0
Frank Luo <luofrank@google.com>
2018-11-20 15:33:34 +0800

    Workaround for sensor ipc message causing high power consume
    Sync from Qcom's document KBA-180725024109
    To avoid the non-wakeup type sensor data break the AP sleep flow,
    notify sensor subsystem in the first place of pm_suspend .
    
    Bug: 118418963
    Test: measure power consumption after running test case
    Change-Id: I2848230d495e30ac462aef148b3f885103d9c24e
    Signed-off-by: Frank Luo <luofrank@google.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/soc/qcom/smp2p_sleepstate.c
+++ drivers/soc/qcom/smp2p_sleepstate.c
@@ -20,8 +20,9 @@
 #include "smp2p_private.h"
 
 #define SET_DELAY (2 * HZ)
-int PROC_AWAKE_ID = 12; /* 12th bit */
+#define PROC_AWAKE_ID 12 /* 12th bit */
 int slst_gpio_base_id;
+
 
 /**
  * sleepstate_pm_notifier() - PM notifier callback function.
@@ -37,12 +38,11 @@ static int sleepstate_pm_notifier(struct notifier_block *nb,
 {
 	switch (event) {
 	case PM_SUSPEND_PREPARE:
-		msleep(25); /* To be tuned based on SMP2P latencies */
+		usleep_range(10000, 10500); /* Tuned based on SMP2P latencies */
 		msm_ipc_router_set_ws_allowed(true);
 		break;
 
 	case PM_POST_SUSPEND:
-		msleep(25); /* To be tuned based on SMP2P latencies */
 		msm_ipc_router_set_ws_allowed(false);
 		break;
 	}
