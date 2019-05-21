8406e01a135a8d0723a2693734a514c77e099704
Tyler Nijmeh <tylernij@gmail.com>
2019-05-20 22:05:21 -0700

    block: anxiety: Add logic for sync block requests
    Some requests would like to be treated as a synchronous request. In this
    case, we can allow these special requests to be part of the READ FIFO
    list. They are treated as READ requests, inheriting their request
    biases. Also, READ requests are always treated as sync.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- block/anxiety-iosched.c
+++ block/anxiety-iosched.c
@@ -75,6 +75,28 @@ static void anxiety_add_request(struct request_queue *q, struct request *rq)
 	struct anxiety_data *adata = q->elevator->elevator_data;
 
 	list_add_tail(&rq->queuelist, &adata->queue[dir]);
+
+	list_add_tail(&rq->queuelist, &((struct anxiety_data *) q->elevator->elevator_data)->queue[dir]);
+}
+
+static struct request *anxiety_former_request(struct request_queue *q, struct request *rq)
+{
+	const uint8_t dir = rq_is_sync(rq);
+
+	if (rq->queuelist.prev == &((struct anxiety_data *) q->elevator->elevator_data)->queue[dir])
+		return NULL;
+
+	return list_prev_entry(rq, queuelist);
+}
+
+static struct request *anxiety_latter_request(struct request_queue *q, struct request *rq)
+{
+	const uint8_t dir = rq_is_sync(rq);
+
+	if (rq->queuelist.next == &((struct anxiety_data *) q->elevator->elevator_data)->queue[dir])
+		return NULL;
+
+	return list_next_entry(rq, queuelist);
 }
 
 static int anxiety_init_queue(struct request_queue *q, struct elevator_type *elv)
