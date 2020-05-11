682b3425d61633f3fb66a02b0eda5d50ef208209
Reinazhard <muh.alfarozy@gmail.com>
2020-05-11 15:06:19 +0700

    Revert "lto: Add Link Time Optimization support for GCC"
    This reverts commit a1df175b37f569684bd40fcc8649650ce139fe72.

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -55,8 +55,12 @@ modpost_link()
 			${KBUILD_VMLINUX_MAIN}				\
 			--end-group"
 	fi
+<<<<<<< HEAD
 	${LDFINAL} ${LDFLAGS} -r -o ${1} ${objects}
 >>>>>>> a1df175b37f5... lto: Add Link Time Optimization support for GCC
+=======
+	${LD} ${LDFLAGS} -r -o ${1} ${objects}
+>>>>>>> parent of a1df175b37f5... lto: Add Link Time Optimization support for GCC
 }
 
 # Link of vmlinux
@@ -82,7 +86,7 @@ vmlinux_link()
 				${1}"
 		fi
 
-		${LDFINAL} ${LDFLAGS} ${LDFLAGS_vmlinux} -o ${2}	\
+		${LD} ${LDFLAGS} ${LDFLAGS_vmlinux} -o ${2}		\
 			-T ${lds} ${objects}
 >>>>>>> a1df175b37f5... lto: Add Link Time Optimization support for GCC
 	else
